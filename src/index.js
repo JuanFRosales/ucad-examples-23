9f1a695f7b7a540a39a47205554042cd58c690af
Juan <juanRosalesMakinen@metropolia.fi>
2023-10-25 10:52:49 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to my REST API!');
+  console.log('request:', req.method, req.url);
+  //    TODO: test method url and generate response accordingly
+  // use e.g. if-else
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Welcome to my REST API!</h1>');
 });
 
 server.listen(port, hostname, () => {
