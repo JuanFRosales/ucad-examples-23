8e87fd0767e20b5adedb2902fc4cb5aa181898ac
Juan <juanRosalesMakinen@metropolia.fi>
2023-11-01 13:40:08 +0200

    addons

--- src/items.js
+++ src/items.js
@@ -5,6 +5,11 @@ const items = [
   {id: 19, name: 'appelsiini'},
 ];
 
+const todos = [
+  { id: 1, text: 'Learn Node.js', completed: false },
+  { id: 2, text: 'Build a REST API', completed: true },
+];
+
 const getItems = (res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
   const jsonItems = JSON.stringify(items);
@@ -95,5 +100,63 @@ const putItem = (req, res, id) => {
       }
     });
 };
+
+/*const getTodos = (res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  const jsonTodos = JSON.stringify(todos);
+  res.end(`{"message": "All tasks": ${jsonTodos}}`);
+};
+
+const addTodo = (req, res) => {
+  let body = [];
+
+  req
+    .on('error', (err) => {
+      console.error(err);
+    })
+    .on('data', (chunk) => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+      console.log('req body', body);
+      body = JSON.parse(body);
+
+      if (!body.text || typeof body.completed !== 'boolean') {
+        // Check if the request body is valid. It should have 'text' and 'completed' fields.
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end('{"message": "Invalid data."}');
+        return;
+      }
+
+      // Create a new to-do item with a unique ID
+      const newTodo = {
+        id: todos.length + 1,
+        text: body.text,
+        completed: body.completed,
+      };
+
+      // Add the new to-do item to the array
+      todos.push(newTodo);
+
+      res.writeHead(201, { 'Content-Type': 'application/json' });
+      res.end('{"message": "New to-do item added."}');
+    });
+};
+
+const deleteTodo = (res, id) => { 
+  const index = todos.findIndex((todo) => todo.id == id);
+
+  if (index !== -1) {
+    // Remove the task from the array
+    todos.splice(index, 1);
+    res.writeHead(204, { 'Content-Type': 'application/json' });
+    res.end('{"message": "Task deleted."}');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end('{"message": "Task not found."}');
+  }
+};
+*/
 
 export { getItems, getItemsById, postItem, deleteItem, putItem };
